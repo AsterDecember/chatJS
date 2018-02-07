db06e13752df1bc32ea69811ceb72f47aa33e9a8
AsterDecember <aster.december@gmail.com>
2018-02-06 22:46:32 -0600

    configurations

---
+++ assets/firebase_config.js
@@ -0,0 +1,10 @@
+console.log('Firebase.js');
+var config = {
+  apiKey: "AIzaSyAOcjxpa6dq_0H-lIUj0WseYO6G8XoGkXI",
+  authDomain: "chatjs-9d06c.firebaseapp.com",
+  databaseURL: "https://chatjs-9d06c.firebaseio.com",
+  projectId: "chatjs-9d06c",
+  storageBucket: "chatjs-9d06c.appspot.com",
+  messagingSenderId: "158814975613"
+};
+firebase.initializeApp(config);
